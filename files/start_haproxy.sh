34d5eedb94478696a04cc1ef7b328539d46b1945
Pierre Chaussalet <pchaussalet@gmail.com>
2015-09-29 04:40:04 +0200

    Add traces

--- files/start_haproxy.sh
+++ files/start_haproxy.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+echo "About to configure haproxy"
 sed -i "s/\$PORT /${PORT}/g" haproxy.cfg
+echo "haproxy configure:"
 cat haproxy.cfg
+echo "About to start haproxy"
 haproxy -f haproxy.cfg -db
